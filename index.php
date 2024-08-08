262cc09ac9c8613867d80c77b11424ae7d68b550
guimelloo <negromonteguilherme@gmail.com>
2024-08-08 12:43:32 +0200

    hidden some infos

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require 'vendor/autoload.php';
 use EmailSender\Crud\Create;
 use EmailSender\Crud\Inputs\TerminalInput;
 use EmailSender\Crud\Outputs\TerminalOutput;
-use EmailSender\Crud\Sender\Email;
+use EmailSender\Crud\Sender\EmailSend;
 use EmailSender\Crud\DataBase\WriteFile;
 
-$crud = new Create(new TerminalInput, new WriteFile(__DIR__ . '/database.txt'), new Email, new TerminalOutput());
+$crud = new Create(new TerminalInput, new WriteFile(__DIR__ . '/database.txt'), new EmailSend, new TerminalOutput());
 
 $crud->run();
 
